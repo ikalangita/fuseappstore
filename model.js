5a804a05dffbd9e79a57c1b21ab1caa4e26eff27
ikalangita <ikalangitahaja@gmail.com>
2018-03-03 13:09:50 +0100

    scroll content

--- model.js
+++ model.js
@@ -11,11 +11,7 @@ module.exports = Observable(
 			{ title: 'Elevation', fact: '3,410 m' },
 			{ title: 'Area', fact: '113 km2' }
 		],
-		facts: [
-			'The Ancient Bristlecone Pine Forest is a protected area high in the White Mountains in Inyo County in eastern California.',
-			'The Great Basin Bristlecone Pine (Pinus longaeva) trees grow between 9,800 and 11,000 feet (3,000–3,400 m) above sea level',
-			"For many years, it was the world's oldest known living non-clonal organism."
-		]
+		facts: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
 	},{
 		id:1,
 		name: 'Iceland',
@@ -26,11 +22,8 @@ module.exports = Observable(
 			{ title: 'Population', fact: '323,002 (2013)' },
 			{ title: 'Currency', fact: 'Icelandic króna'}
 		],
-		facts: [
-			'Icelandic is a Nordic island country in the North Atlantic Ocean.',
-			'According to Landnámabók, the settlement of Iceland began in the year 874 AD when the Norwegian chieftain Ingólfr Arnarson became the first permanent settler on the island.',
-			'Iceland has a market economy with relatively low taxes compared to other OECD countries.'
-		]
+		facts: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+
 	},{
 		id: 2,
 		name: 'Monte Altissimo di Nago',
@@ -41,11 +34,8 @@ module.exports = Observable(
 			{ title: 'Elevation', fact: '2,074 m' },
 			{ title: 'Province', fact: 'Trentino' }
 		],
-		facts:  [
-			'Monte Altissimo di Nago is one of the highest summits of the Monte Baldo mountain range and thereby part of the Garda Mountains in northern Italy.',
-			'The Altissimo is the highest peak in the northern part of the Monte-Baldo range.',
-			'On top of the Altissimo there is a mountain hut, the Rifugio Damiano Chiesa. The easiest way to reach the top is a hike over a dirt road from Strada Provinciale del Monte Baldo.'
-		]
+		facts: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+
 	},{
 		id: 3,
 		name: 'Setagaya, Tokyo',
@@ -56,11 +46,8 @@ module.exports = Observable(
 			{ title: 'Population', fact: '890,927' },
 			{ title: 'Area', fact: '58.08 km2' }
 		],
-		facts:  [
-			'Setagaya is a special ward in Tokyo, Japan.',
-			"Setagaya has the largest population and second largest area (after Ōta) of Tokyo's 23 special wards.",
-			'Setagaya is located at the southwestern corner of the 23 special wards and Tama River separates the boundary between Tokyo Metropolis and Kanagawa Prefecture.'
-		]
+		facts: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+
 	},{
 		id: 4,
 		name: 'Provo Canyon',
@@ -71,11 +58,8 @@ module.exports = Observable(
 			{ title: 'From', fact: 'Orem' },
 			{ title: 'To', fact: 'Heber City' }
 		],
-		facts: [
-			'Provo Canyon is a canyon located in unincorporated Utah County, Utah, USA.',
-			'Provo Canyon splits between Mount Timpanogos on the north and Mount Cascade on the south.',
-			'Attractions in Provo Canyon include Vivian Park and Bridal Veil Falls.'
-		]
+		facts: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+
 	},{
 		id: 5,
 		name: 'Ciucaș Peak',
@@ -86,8 +70,7 @@ module.exports = Observable(
 			{ title: 'Elevation', fact: '1,954 m' },
 			{ title: 'Parent range', fact: 'Ciucaş Mountains' }
 		],
-		facts:  [
-			'Ciucaş Peak (Romanian: Vârful Ciucaş) is the highest peak of Ciucaş Mountains, Southern Carpathians.',
-		]
+		facts: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+
 	}
 );
